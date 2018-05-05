6c37d29199f03a3fc799cbb5d5bba422daa393d6
Cale Woodley <inbox@cale.io>
2018-05-05 11:43:34 +0800

    App component controls data state

--- src/app.js
+++ src/app.js
@@ -1,10 +1,50 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import io from "socket.io-client";
 
 import { Game } from "./components/Game";
 
-const App = (props, context) => {
-  return <Game />
+const socketEndpoint = "http://cale.localdev:3000";
+
+class App extends React.Component {
+  state = {
+    connected: false,
+    currentGame: null
+  };
+
+  connectToSocket() {
+    const socket = io(socketEndpoint);
+
+    socket.on("connect", () => {
+      this.setState({ connected: true });
+    });
+
+    socket.on("currentGame", data => {
+      this.setCurrentGame(data);
+    });
+  }
+
+  setCurrentGame(data) {
+    if (this.state.currentGame === data) {
+      return;
+    }
+
+    this.setState({ currentGame: data });
+  }
+
+  componentDidMount() {
+    this.connectToSocket();
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>{this.state.connected ? "connected" : "disconnected"}</div>
+
+        <Game currentGame={this.state.currentGame} nextGame={false} />
+      </React.Fragment>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(<App />, document.getElementById("app"));
--- src/components/Game.js
+++ src/components/Game.js
@@ -2,38 +2,22 @@
 
 import * as React from "react"
 import * as ReactDom from "react-dom"
-import io from "socket.io-client"
 
-const socketEndpoint = 'http://localhost:3000/update'
-
-type Props = {}
-
-type State = {
-  currentGame: string | null
+type Props = {
+  currentGame: string,
+  nextGame: string
 }
 
-export class Game extends React.Component<Props,State> {
-  state = {
-    currentGame: null
-  }
-
-  connectToSocket() {
-    const socket = io(socketEndpoint)
-
-    socket.on("text", (data) => console.log(data));
-
-  }
-
-  componentDidMount() {
-    this.connectToSocket()
-  }
+export class Game extends React.Component<Props> {
 
   render() {
+    const {currentGame, nextGame} = this.props
+
     return (
       <React.Fragment>
-        <h1>titles</h1>    
+        <h1>titles</h1>
 
-        <h2>game: {this.state.currentGame}</h2>
+        <h2>game: {currentGame}</h2>
       </React.Fragment>
     )
   }
