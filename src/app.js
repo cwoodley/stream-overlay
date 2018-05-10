4f16d8585f6626e769d1c222beda2c2881b1f28f
Cale Woodley <inbox@cale.io>
2018-05-10 17:41:15 +0800

    Prettier changes

--- src/app.js
+++ src/app.js
@@ -1,87 +1,70 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import queryString from "query-string";
-import io from "socket.io-client";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import queryString from 'query-string'
+import io from 'socket.io-client'
 
-import {
-  Game
-} from "./components/Game";
+import { Game } from './components/Game'
 
-const socketEndpoint = "http://cale.localdev:3000";
+const socketEndpoint = 'http://cale.localdev:3000'
 
 class App extends React.Component {
   state = {
     connected: false,
     currentGame: null,
     debugStyling: false
-  };
+  }
 
   connectToSocket() {
-    const socket = io(socketEndpoint);
+    const socket = io(socketEndpoint)
 
-    socket.on("connect", () => {
+    socket.on('connect', () => {
       this.setState({
         connected: true
-      });
-    });
+      })
+    })
 
-    socket.on("currentGame", data => {
+    socket.on('currentGame', data => {
       this.setState({
         currentGame: data
-      });
-    });
+      })
+    })
 
-    socket.on("debug", data => {
+    socket.on('debug', data => {
       console.log(data)
     })
   }
 
   checkDebugFlags() {
-    const flags = location.search;
+    const flags = location.search
 
     if (!flags) {
-      return;
+      return
     }
 
-    const parsed = queryString.parse(flags);
+    const parsed = queryString.parse(flags)
     if (!parsed) {
-      return;
-    } else if (parsed.debug === "styles") {
+      return
+    } else if (parsed.debug === 'styles') {
       this.setState({
         debugStyling: true
-      });
+      })
     }
   }
 
   componentDidMount() {
-    this.checkDebugFlags();
-    this.connectToSocket();
+    this.checkDebugFlags()
+    this.connectToSocket()
   }
 
   render() {
-    return ( <
-      React.Fragment >
-      <
-      div > {
-        this.state.connected ? "connected" : "disconnected"
-      } < /div>
-
-      <
-      Game currentGame = {
-        this.state.currentGame
-      }
-      nextGame = {
-        false
-      }
-      />
-
-      <
-      div > {
-        this.state.debugStyling ? "debug styling on" : "false"
-      } < /div> <
-      /React.Fragment>
-    );
+    return (
+      <React.Fragment>
+        <div> {this.state.connected ? 'connected' : 'disconnected'} </div>
+        <Game currentGame={this.state.currentGame} nextGame={false} />
+        {/* <div>{this.state.debugStyling ? 'debug styling on' : 'false'}</div> */}
+      </React.Fragment>
+    )
   }
 }
 
-ReactDOM.render( < App / > , document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'))
