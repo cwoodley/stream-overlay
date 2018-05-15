9501395d67c6553135ab164c7c164bc595c6d47f
Cale Woodley <inbox@cale.io>
2018-05-15 19:57:04 +0800

    Fixes deps, adds flow support to app

--- src/app.js
+++ src/app.js
@@ -1,42 +1,53 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import queryString from 'query-string'
-import io from 'socket.io-client'
+/* @flow */
 
-import { Game } from './components/Game'
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import queryString from "query-string"
+import io from "socket.io-client"
 
-const socketEndpoint = 'http://cale.localdev:3000'
+import { Game } from "./components/Game"
 
-class App extends React.Component {
+const socketEndpoint = "http://cale.localdev:3000"
+
+type State = {
+  connected: boolean,
+  currentGame: string,
+  nextGame: string,
+  debugStyling: boolean,
+  tickerItems: string[]
+}
+
+export class App extends React.Component<{}, State> {
   state = {
     connected: false,
-    currentGame: null,
+    currentGame: "",
+    nextGame: "",
     debugStyling: false,
-    tickerItems: null
+    tickerItems: []
   }
 
   connectToSocket() {
     const socket = io(socketEndpoint)
 
-    socket.on('connect', () => {
+    socket.on("connect", () => {
       this.setState({
         connected: true
       })
     })
 
-    socket.on('currentGame', data => {
+    socket.on("currentGame", data => {
       this.setState({
         currentGame: data
       })
     })
 
-    socket.on('tickerItems', data => {
+    socket.on("tickerItems", data => {
       this.setState({
         tickerItems: data
       })
     })
 
-    socket.on('debug', data => {
+    socket.on("debug", data => {
       console.log(data)
     })
   }
@@ -51,7 +62,7 @@ class App extends React.Component {
     const parsed = queryString.parse(flags)
     if (!parsed) {
       return
-    } else if (parsed.debug === 'styles') {
+    } else if (parsed.debug === "styles") {
       this.setState({
         debugStyling: true
       })
@@ -66,14 +77,15 @@ class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div> {this.state.connected ? 'connected' : 'disconnected'} </div>
+        <div> {this.state.connected ? "connected" : "disconnected"} </div>
         <Game
           currentGame={this.state.currentGame}
           nextGame={this.state.nextGame}
         />
+        {/* <Ticker tickerItems={this.state.tickerItems} /> */}
       </React.Fragment>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, (document.getElementById("app"): any))
