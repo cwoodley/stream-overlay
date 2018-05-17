c8685ea7779d22551a0c3c5b0b6a4dd06df1798e
Cale Woodley <inbox@cale.io>
2018-05-17 17:14:14 +0800

    Creates countdown component

---
+++ src/components/Countdown.js
@@ -0,0 +1,88 @@
+/* @flow */
+
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+
+type Props = {
+  deadline: Date
+}
+
+type State = {
+  hours: number,
+  minutes: number
+}
+
+export class Countdown extends React.Component<Props, State> {
+  state = {
+    hours: 0,
+    minutes: 0
+  }
+
+  getTimeRemaining = (deadline: Date) => {
+    const now = new Date()
+    const timeEnd = deadline.toJSON()
+    const timeStart = now.toJSON()
+
+    // r = remaining time in milliseconds
+    const r = Date.parse(timeEnd) - Date.parse(timeStart)
+    const minutes = Math.floor((r / (1000 * 60)) % 60)
+    const hours = Math.floor((r / (1000 * 60 * 60)) % 60)
+
+    return {
+      hours: hours,
+      minutes: minutes
+    }
+  }
+
+  runClock = (hours: number, minutes: number) => {
+    const complete = hours <= 0 && minutes <= 0
+
+    const clock = setInterval(() => {
+      this.setState({
+        hours: hours,
+        minutes: minutes
+      })
+
+      if (complete) {
+        clearInterval(clock)
+        this.setState({
+          hours: 0,
+          minutes: 0
+        })
+      }
+    }, 1000 * 30)
+  }
+
+  setClock = (deadline: Date) => {
+    const hours = this.getTimeRemaining(deadline).hours
+    const minutes = this.getTimeRemaining(deadline).minutes
+    const complete = hours <= 0 && minutes <= 0
+
+    if (!complete) {
+      this.setState(
+        {
+          hours: hours,
+          minutes: minutes
+        },
+        this.runClock(hours, minutes)
+      )
+    }
+  }
+
+  componentDidMount() {
+    this.setClock(this.props.deadline)
+  }
+
+  render() {
+    const hours = this.state.hours
+    const minutes = this.state.minutes
+    const complete = hours <= 0 && minutes <= 0
+
+    return (
+      <div>
+        {!complete && `${this.state.hours}hrs, ${this.state.minutes}mins`}
+        {complete && `Time's up!`}
+      </div>
+    )
+  }
+}
